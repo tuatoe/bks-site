2f22ed88c6a84b0a416e8bce1442a53315fd70d6
Tuatoe <tuatoe.daynuah@gmail.com>
2017-06-30 01:08:51 -0500

    site initial build

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 //first argument task name, second what we want it to do
 gulp.task('default', function(){
@@ -10,9 +14,21 @@ gulp.task('html',function(){
     console.log('something cool');
 });
 
+//styles task
+gulp.task('styles',function(){
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssvars,nested,autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
+});
+
+
 gulp.task('watch',function(){
     //first argument file we want to watch, second what we want it to do
     watch('./app/index.html', function(){
         gulp.start('html');
     });
+
+    watch('./app/assets/styles/**/*.css', function(){
+        gulp.start('styles');
+    })
 });
