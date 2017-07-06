ad5e99da34711be051837d56efa76f9058579c93
Tuatoe <tuatoe.daynuah@gmail.com>
2017-07-06 17:10:41 -0500

    complete reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,14 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+
+//reveal our team section
+new RevealOnScroll($('.our-team'), '85%');
+
+//reveal what we do section
+//new RevealOnScroll($('.reveal-what-we-do'), '70%');
+
+//reveal about bks section
+//new RevealOnScroll($('.reveal-about-bks'), '70%');
